fe561ddeb48f7b57387ae1278a885d70ff9974d3
mo-ro <gainmoro@gmail.com>
2018-06-20 16:36:12 +0900

    init game

---
+++ game/gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var browser = require("browser-sync");
+var plumber = require("gulp-plumber");
+
+// gulp.task("html", function() {
+//   gulp.src(["**/*.html", "!**/node_modules/**/*.scss"])
+//     // .pipe(browser.reload({stream:true}))
+// });
+
+gulp.task("sass", function() {
+  gulp.src(["**/*.scss", "!**/node_modules/**/*.scss"])
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("."))
+    // .pipe(browser.reload({stream:true}))
+});
+
+// gulp.task("js", function() {
+//   gulp.src("**/*.js")
+//     // .pipe(browser.reload({stream:true}))
+// });
+
+gulp.task("server", function() {
+  browser({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+gulp.task("default",['server'], function() {
+  // gulp.watch("**/*.html",["html"]);
+  // gulp.watch("**/*.js",["js"]);
+  gulp.watch("**/*.scss",["sass"]);
+});
