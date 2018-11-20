5196d01899165f7345608efa4e88482301a6753e
mo-ro <gainmoro@gmail.com>
2018-11-19 23:15:38 -0800

    completed

--- dribbble/switcherMenu/js/main.js
+++ dribbble/switcherMenu/js/main.js
@@ -1,26 +1,56 @@
 var degree = 0;
+var counter = 0;
 
 $(".select-box").on("mousedown", function() {
   TweenMax.to($(".arrow"), 0.2, {
     opacity: 1
   });
 
-  TweenMax.fromTo($(".cover"), 0.2, {
-    borderWidth: 22,
-    height: 0,
-    scale: 1,
-    width: 0,
-  },
-  {
-    scale: 0.8,
-    opacity: 0.2,
-  })
+  TweenMax.fromTo(
+    $(".cover"),
+    0.2,
+    {
+      borderWidth: 22,
+      height: 0,
+      scale: 1,
+      width: 0
+    },
+    {
+      scale: 0.8,
+      opacity: 0.14
+    }
+  );
 });
 
 $(".select-box").on("mouseup", function(event) {
   $(this).css({
-    pointerEvents: 'none'
-  })
+    pointerEvents: "none"
+  });
+
+  counter++;
+  TweenMax.fromTo(
+    $(".-item-" + (counter % 4)),
+    0.45,
+    {
+      opacity: 0
+    },
+    {
+      opacity: 1,
+      ease: Power0.easeIn
+    }
+  );
+
+  TweenMax.fromTo(
+    $(".-item-" + ((counter % 4) - 1)),
+    0.45,
+    {
+      opacity: 1
+    },
+    {
+      opacity: 0,
+      ease: Power1.easeOut
+    }
+  );
 
   TweenMax.to($(".arrow"), 0.2, {
     opacity: 0.4
@@ -37,18 +67,20 @@ $(".select-box").on("mouseup", function(event) {
     delay: 0.2
   });
 
-  TweenMax.to($(".select-list"), 0.45, {
-    rotation: degree += 90
+  TweenMax.to($(".select-list"), 0.4, {
+    rotation: (degree += 90),
+    ease: Power0.easeIn
   });
 
-  TweenMax.to(this, 0.25, {
+  TweenMax.to(this, 0.3, {
     rotation: 5,
     repeat: 1,
+    ease: Power0.easeOut,
     yoyo: true,
     onComplete: function() {
       $(this.target).css({
-        pointerEvents: 'all'
-      })
+        pointerEvents: "all"
+      });
     }
-  })
+  });
 });
